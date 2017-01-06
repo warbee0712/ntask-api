e63a9b1a84c9614a6d829af52782b14c4ed092ed
warbee0712 <warbee0712@outlook.com>
2017-01-06 15:40:33 -0200

    Setup CORS

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,8 +1,14 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import cors from 'cors'
 
 module.exports = app => {
   app.set('port', 3000)
+  app.use(cors({
+    origin: ['http://localhost:3001/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
   app.use(bodyParser.json())
   app.use(app.auth.initialize())
   app.use((req, res, next) => {
