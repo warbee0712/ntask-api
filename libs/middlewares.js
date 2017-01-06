61a90128d51766d92d5f61aed4fa2fa36a8e9be0
warbee0712 <warbee0712@outlook.com>
2017-01-06 15:58:28 -0200

    Add API logging

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,18 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
+import logger from './logger.js'
 
 module.exports = app => {
   app.set('port', 3000)
+  app.use(morgan('common', {
+    stream: {
+      write: (message) => {
+        logger.info(message)
+      }
+    }
+  }))
   app.use(cors({
     origin: ['http://localhost:3001/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
